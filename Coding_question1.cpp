fab1cbaa4aae36a595ce1284497d570d10f8a411
Deepti Chauhan <76423762+deepti-chauhan@users.noreply.github.com>
2022-10-29 20:08:49 +0530

    Create Coding_question1.cpp

---
+++ Coding_question1.cpp
@@ -0,0 +1,33 @@
+vector<vector<int>> func(int n,vector<int> left,vector<int> right)
+{
+    vector<vector<int>> intervals(n);
+    for(int i=0;i<n;i++)
+    {
+        intervals[i]={left[i],right[i]};
+    }
+    sort(intervals.begin(),intervals.end());
+    vector<vector<int>> output;
+    output.push_back(intervals[0]);
+    for(int i=1; i<n; i++) {
+        
+        if(output.back()[1] >= intervals[i][0])
+            output.back()[1] = max(output.back()[1] , intervals[i][1]);
+        else
+            output.push_back(intervals[i]); 
+        }
+        
+    vector<vector<int>> result;
+    int prev=0;
+    for(int i=0;i<n;i++)
+    {
+        if(prev!=output[i][0])
+        {
+            result.push_back({prev,output[i][0]});
+        }
+        prev=output[i][1];
+    }
+    if(prev!=1000000000)
+        result.push_back({prev,1000000000});
+    
+    return result;
+}
